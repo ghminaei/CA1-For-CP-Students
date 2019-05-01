4e88bd431ea83e07eefdf6c6165c37d031882275
Ghazal <ghminaei1037@gmail.com>
2019-05-01 22:29:39 +0430

    initial files added

---
+++ HW3Dict.py
@@ -0,0 +1,10 @@
+game1 = {'Name': 'God of war', 'Genre': 'Action adventure', 'Metascore': 94, 'UsersScores': [4, 3, 4, 3, 0, 1, 9, 7, 6]}
+game2 = {'Name': 'Tomb raider', 'Genre': 'Action adventure', 'Metascore': 86, 'UsersScores': [10, 9, 8, 10, 9, 7, 6, 5, 10]}
+game3 = {'Name': 'Rayman Legends', 'Genre': 'Platformer', 'Metascore': 90, 'UsersScores': [10, 9, 8, 7, 10, 7, 6, 10, 9]}
+game4 = {'Name': 'Detroit: Become Human', 'Genre': 'Adventure', 'Metascore': 78, 'UsersScores': [6, 10, 7, 9, 10, 3, 9, 10, 10]}
+game5 = {'Name': 'Horizon zero dawn', 'Genre': 'Open World', 'Metascore': 89, 'UsersScores': [9, 8, 7, 10, 9, 8, 6, 2, 10]}
+game6 = {'Name': 'Red dead redemption2', 'Genre': 'Open World', 'Metascore': 97, 'UsersScores': [7, 8, 5, 6, 9, 5 ,8 ,4, 8]}
+game7 = {'Name': 'Uncharted: the lost legacy', 'Genre': 'Action adventure', 'Metascore': 84, 'UsersScores': [9, 10, 10, 10, 5, 6, 10, 9, 8]}
+game8 = {'Name': 'Life is strange', 'Genre': 'Adventure', 'Metascore': 83, 'UsersScores': [0, 5, 4, 7, 9, 10, 4, 3, 0]}
+game9 = {'Name': 'Little big planet3', 'Genre': 'Platformer', 'Metascore': 79, 'UsersScores': [6, 10, 7, 8, 6, 1, 6, 6, 2]}
+game10 = {'Name': 'Just cause4', 'Genre': 'Open World', 'Metascore': 65, 'UsersScores': [3, 0, 0, 3, 3, 5, 1, 5, 4]}
