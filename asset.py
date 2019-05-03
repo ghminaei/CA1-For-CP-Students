4db42cbb15edbf0caa90befcaa8dfe732b5c7797
Ghazal <ghminaei1037@gmail.com>
2019-05-03 11:17:23 +0430

    nothing special:)

---
+++ asset.py
@@ -0,0 +1,35 @@
+def average(): #you may change arguments
+    #get a list of numbers and return avg
+    #your code#
+    pass
+
+def standardDeviation(): #you may change arguments
+    #get a list of numbers and calculate st 
+    #your code#
+    pass
+
+def insertionSort(): #you may change arguments
+    #implement insertion sort for a given list
+    # https://en.wikipedia.org/wiki/Insertion_sort
+    #your code#
+    pass
+
+def isIthGamesEqual(): #you may change arguments
+    #your code#
+    pass
+
+
+#This information is based on metacritic.com, but some of them are changed!
+game1 = {'Name': 'God of war', 'Genre': 'Action adventure', 'Metascore': 94, 'UsersScores': [4, 3, 4, 3, 0, 1, 9, 7, 6]}
+game2 = {'Name': 'Tomb raider', 'Genre': 'Action adventure', 'Metascore': 86, 'UsersScores': [10, 9, 8, 10, 9, 7, 6, 5, 10]}
+game3 = {'Name': 'Rayman Legends', 'Genre': 'Platformer', 'Metascore': 90, 'UsersScores': [10, 9, 8, 7, 10, 7, 6, 10, 9]}
+game4 = {'Name': 'Detroit: Become Human', 'Genre': 'Adventure', 'Metascore': 78, 'UsersScores': [6, 10, 7, 9, 10, 3, 9, 10, 10]}
+game5 = {'Name': 'Horizon zero dawn', 'Genre': 'Open World', 'Metascore': 89, 'UsersScores': [9, 8, 7, 10, 9, 8, 6, 2, 10]}
+game6 = {'Name': 'Red dead redemption2', 'Genre': 'Open World', 'Metascore': 97, 'UsersScores': [7, 8, 5, 6, 9, 5 ,8 ,4, 8]}
+game7 = {'Name': 'Uncharted: the lost legacy', 'Genre': 'Action adventure', 'Metascore': 84, 'UsersScores': [9, 10, 10, 10, 5, 6, 10, 9, 8]}
+game8 = {'Name': 'Life is strange', 'Genre': 'Adventure', 'Metascore': 83, 'UsersScores': [0, 5, 4, 7, 9, 10, 4, 3, 0]}
+game9 = {'Name': 'Little big planet3', 'Genre': 'Platformer', 'Metascore': 79, 'UsersScores': [6, 10, 7, 8, 6, 1, 6, 6, 2]}
+game10 = {'Name': 'Just cause4', 'Genre': 'Open World', 'Metascore': 65, 'UsersScores': [3, 0, 0, 3, 3, 5, 1, 5, 4]}
+
+#your code#
+#you may add some functions as well
